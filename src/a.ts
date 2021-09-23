c598c381899c7c5b350f382d7f757d962fed2086
Bishal9 <goonbishal.bg@gmail.com>
2021-09-23 16:09:17 +0530

    Multiples of 3 or 5

---
+++ src/a.ts
@@ -0,0 +1,19 @@
+/*
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+*/
+
+let sum: number = 0
+
+for (let i: number = 3; i < 1000; i++) {
+
+    if (i % 3 === 0 || i % 5 === 0) {
+
+        sum += i
+    }
+}
+
+console.log(`Sum is ${sum}`)
